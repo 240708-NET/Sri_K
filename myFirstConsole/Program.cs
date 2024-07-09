ec91c883d388155235e854fea5a9b767d240788d
sxk232 <127258722+sxk232@users.noreply.github.com>
2024-07-09 16:34:58 -0700

    First C# project

---
+++ myFirstConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class UserInput
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");  // Use Write for no newline after prompt
+        string userInput = Console.ReadLine();  // Read the user input
+
+        Console.WriteLine(" Welcome to Revature " + userInput);  // Print with formatting
+
+    }
+}
+
+
